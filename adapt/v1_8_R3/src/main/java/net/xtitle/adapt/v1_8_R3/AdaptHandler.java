49570947b8174a6a73599d26591082e6ff4262fb
InitSync <benjaalejandro206@gmail.com>
2022-12-22 13:59:59 -0300

    feat(adapts): add new adapt class

---
+++ adapt/v1_8_R3/src/main/java/net/xtitle/adapt/v1_8_R3/AdaptHandler.java
@@ -0,0 +1,52 @@
+package net.xtitle.adapt.v1_8_R3;
+
+import net.minecraft.server.v1_8_R3.IChatBaseComponent;
+import net.minecraft.server.v1_8_R3.PacketPlayOutChat;
+import net.minecraft.server.v1_8_R3.PacketPlayOutTitle;
+import net.minecraft.server.v1_8_R3.PlayerConnection;
+import net.xtitle.api.adapt.SimpleAdapt;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+public class AdaptHandler
+implements SimpleAdapt {
+	public AdaptHandler() {}
+	
+	/**
+	 * Send a title with times settings.
+	 *
+	 * @param player   Player object.
+	 * @param title    Title message.
+	 * @param subtitle Subtitle message.
+	 * @param fadeIn   In-coming time.
+	 * @param stay     Staying time.
+	 * @param fadeOut  Outing time.
+	 */
+	@Override
+	public void sendTitle(Player player, String title, String subtitle, int fadeIn, int stay, int fadeOut) {
+		PlayerConnection connection = ((CraftPlayer) player).getHandle().playerConnection;
+		
+		connection.sendPacket(new PacketPlayOutTitle(fadeIn, stay, fadeOut));
+		connection.sendPacket(new PacketPlayOutTitle(
+			 PacketPlayOutTitle.EnumTitleAction.TITLE,
+			 IChatBaseComponent.ChatSerializer.a("{\"text\": \"" + title + "\"}"))
+		);
+		connection.sendPacket(new PacketPlayOutTitle(
+			 PacketPlayOutTitle.EnumTitleAction.SUBTITLE,
+			 IChatBaseComponent.ChatSerializer.a("{\"text\": \"" + subtitle + "\"}"))
+		);
+	}
+	
+	/**
+	 * Send an actionbar to player.
+	 *
+	 * @param player  Player object.
+	 * @param message Message to send.
+	 */
+	@Override
+	public void sendActionBar(Player player, String message) {
+		((CraftPlayer) player).getHandle()
+			 .playerConnection
+			 .sendPacket(new PacketPlayOutChat(IChatBaseComponent.ChatSerializer.a("{\"text\": \"" + message + "\"}"), (byte) 2));
+	}
+}
