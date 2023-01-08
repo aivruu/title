e1cdf2519a2e660b087d31cb9db1df3ab2467485
InitSync <benjaalejandro206@gmail.com>
2023-01-07 21:42:54 -0300

    chore(SimpleTitleManager): remove comments from interface, implement interface model changes and add static imports

--- lib/src/main/java/net/xtitle/lib/managers/SimpleTitleManager.java
+++ lib/src/main/java/net/xtitle/lib/managers/SimpleTitleManager.java
@@ -4,16 +4,22 @@ import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.xtitle.api.TitleManager;
 import net.xtitle.api.adapt.SimpleAdapt;
-import net.xtitle.lib.XTitle;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkArgument;
+import static net.xtitle.lib.XTitle.canSupport;
 
 public class SimpleTitleManager
 implements TitleManager {
 	private final SimpleAdapt adapt;
 	
-	public SimpleTitleManager(SimpleAdapt adapt) {
+	public SimpleTitleManager(@Nullable SimpleAdapt adapt) {
 		this.adapt = adapt;
 	}
 	
@@ -23,10 +29,15 @@ implements TitleManager {
 	 * @param player Player object.
 	 */
 	@Override
-	public void clearTitle(Player player) {
+	public void clearTitle(@Nonnull Player player) {
+		checkNotNull(player, "The player can't be null.");
+		
 		// Checks if the server version is equals or higher to 1.17
-		if (XTitle.canSupport(17)) player.resetTitle();
-		else adapt.clearTitle(player);
+		if (canSupport(17)) player.resetTitle();
+		else {
+			assert adapt != null;
+			adapt.clearTitle(player);
+		}
 	}
 	
 	/**
@@ -37,10 +48,20 @@ implements TitleManager {
 	 * @param subtitle Subtitle message.
 	 */
 	@Override
-	public void sendTitle(Player player, String title, String subtitle) {
+	public void sendTitle(@Nonnull Player player, @Nonnull String title, @Nonnull String subtitle) {
+		checkNotNull(player, "The player can't be null.");
+		checkNotNull(title, "The title message can't be null.");
+		checkNotNull(subtitle, "The subtitle message is null.");
+		
+		checkArgument(!title.isEmpty(), "The title can't be empty. If you want to send an empty title use the clearTitle() method.");
+		checkArgument(!subtitle.isEmpty(), "The title can't be empty. If you want to send an empty title use the clearTitle() method.");
+		
 		// Checks if the server version is equals or higher to 1.17
-		if (XTitle.canSupport(17)) player.sendTitle(title, subtitle, 20, 60, 20);
-		else adapt.sendTitle(player, title, subtitle);
+		if (canSupport(17)) player.sendTitle(title, subtitle, 20, 60, 20);
+		else {
+			assert adapt != null;
+			adapt.sendTitle(player, title, subtitle);
+		}
 	}
 	
 	/**
@@ -54,37 +75,57 @@ implements TitleManager {
 	 * @param subtitle Subtitle message.
 	 */
 	@Override
-	public void sendTitle(Player player, int fadeIn, int stay, int fadeOut, String title, String subtitle) {
+	public void sendTitle(@Nonnull Player player, int fadeIn, int stay, int fadeOut, @Nonnull String title, @Nonnull String subtitle) {
+		checkNotNull(player, "The player can't be null.");
+		checkNotNull(title, "The title message can't be null.");
+		checkNotNull(subtitle, "The subtitle message is null.");
+		
+		checkArgument(!title.isEmpty(), "The title can't be empty. If you want to send an empty title use the clearTitle() method.");
+		checkArgument(!subtitle.isEmpty(), "The title can't be empty. If you want to send an empty title use the clearTitle() method.");
+		
 		// Checks if the server version is equals or higher to 1.17
-		if (XTitle.canSupport(17)) player.sendTitle(title, subtitle, fadeIn, stay, fadeOut);
-		else adapt.sendTitle(player, title, subtitle, fadeIn, stay, fadeOut);
+		if (canSupport(17)) player.sendTitle(title, subtitle, fadeIn, stay, fadeOut);
+		else {
+			assert adapt != null;
+			adapt.sendTitle(player, title, subtitle, fadeIn, stay, fadeOut);
+		}
 	}
 	
-	/**
-	 * Sends an actionbar to player without a duration.
-	 *
-	 * @param player Player object.
-	 * @param message ActionBar message.
-	 */
 	@Override
-	public void sendActionBar(Player player, String message) {
+	public void sendTabList(Player player, String header, String footer) {
+		checkNotNull(player, "The player can't be null.");
+		
 		// Checks if the server version is equals or higher to 1.17
-		if (XTitle.canSupport(17)) player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(message));
-		else adapt.sendActionBar(player, message);
+		if (canSupport(17)) player.setPlayerListHeaderFooter(header, footer);
+		else {
+			assert adapt != null;
+			adapt.sendTabList(player, header, footer);
+		}
 	}
 	
-	/**
-	 * Sends an actionbar to player with a ticks duration.
-	 *
-	 * [!] Method code taken out from XSeries repository.
-	 *
-	 * @param plugin A JavaPlugin object/instance.
-	 * @param player Player object.
-	 * @param message The actionbar message.
-	 * @param duration The duration.
-	 */
 	@Override
-	public void sendActionBar(JavaPlugin plugin, Player player, String message, long duration) {
+	public void sendActionBar(@Nonnull Player player, @Nonnull String message) {
+		checkNotNull(player, "The subtitle message is null.");
+		checkNotNull(message, "The actionbar message can't be null.");
+		
+		checkArgument(!message.isEmpty(), "The actionbar message can't be empty.");
+		
+		// Checks if the server version is equals or higher to 1.17
+		if (canSupport(17)) player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(message));
+		else {
+			assert adapt != null;
+			adapt.sendActionBar(player, message);
+		}
+	}
+	
+	@Override
+	public void sendActionBar(@Nonnull JavaPlugin plugin, @Nonnull Player player, @Nonnull String message, long duration) {
+		checkNotNull(plugin, "The JavaPlugin instance is null.");
+		checkNotNull(player, "The subtitle message is null.");
+		checkNotNull(message, "The actionbar message can't be null.");
+		
+		checkArgument(!message.isEmpty(), "The actionbar message can't be empty.");
+		
 		// Checks if the duration value is minor than '1'.
 		if (duration < 1) return;
 		
@@ -94,8 +135,11 @@ implements TitleManager {
 			@Override
 			public void run() {
 				// Checks if the server version is equals or higher to 1.17
-				if (XTitle.canSupport(17)) player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(message));
-				else adapt.sendActionBar(player, message);
+				if (canSupport(17)) player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(message));
+				else {
+					assert adapt != null;
+					adapt.sendActionBar(player, message);
+				}
 				
 				repeater -= 40L;
 				if (repeater - 40L < -20L) cancel();
