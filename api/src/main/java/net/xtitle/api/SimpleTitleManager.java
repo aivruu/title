242ba79f5f400a6d0950ae02d3963733b97dba00
Benjamín <benjaalejandro206@gmail.com>
2023-01-10 02:35:47 -0300

    chore(SimpleTitleManager): invert parameters order on `sendTabList()` method

--- api/src/main/java/net/xtitle/api/SimpleTitleManager.java
+++ api/src/main/java/net/xtitle/api/SimpleTitleManager.java
@@ -65,7 +65,7 @@ public class SimpleTitleManager implements TitleManager {
 	}
 	
 	@Override
-	public void sendTabList(Player player, String header, String footer) {
+	public void sendTabList(Player player, String footer, String header) {
 		checkNotNull(player, "The player can't be null.");
 		
 		// Checks if the server version is equals or higher to 1.17
