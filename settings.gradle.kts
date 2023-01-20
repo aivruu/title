151b2207a975dc56720f9a342bdf101c945796f6
InitSync <benjaalejandro206@gmail.com>
2023-01-20 14:37:29 -0300

    chore: remove old subproject paths (adapts)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,11 +2,8 @@ rootProject.name = "XTitle"
 
 include("api")
 includePrefixed(
-	 "v1_8_R1", "v1_8_R2", "v1_8_R3",
-	 "v1_9_R1", "v1_9_R2", "v1_10_R1",
-	 "v1_11_R1", "v1_12_R1", "v1_13_R2",
-	 "v1_14_R1", "v1_15_R1", "v1_16_R1",
-	 "v1_16_R2", "v1_16_R3"
+	 "v1_8_R1", "v1_8_R2", "v1_8_R3", "v1_9_R1",
+	 "v1_9_R2", "v1_10_R1", "v1_11_R1", "v1_12_R1"
 )
 
 fun includePrefixed(vararg paths: String) {
