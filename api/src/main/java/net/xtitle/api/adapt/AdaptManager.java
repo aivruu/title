128604b5f3c9bb77bdccfec6635c81890472de09
InitSync <benjaalejandro206@gmail.com>
2023-02-19 01:33:25 -0300

    Modify return type for the methods to her own instance

--- api/src/main/java/net/xtitle/api/adapt/AdaptManager.java
+++ api/src/main/java/net/xtitle/api/adapt/AdaptManager.java
@@ -8,13 +8,16 @@ public interface AdaptManager {
 	 * Set up the adapt correspond to use.
 	 *
 	 * @param adapt The adapt for the server version that will be use.
+	 * @return This instance if the adapt was installed or null if was not installed.
 	 */
-	void adapt(SimpleAdapt adapt);
+	AdaptManager adapt(SimpleAdapt adapt);
 	
 	/**
 	 * Search automatically the adapt correspond to the server version for use.
+	 *
+	 * @return This instance if the adapt was installed or null if was not installed.
 	 */
-	void findAdapt();
+	AdaptManager findAdapt();
 	
 	/**
 	 * Returns the current SimpleData object. This will be return null if there not set an adapt.
