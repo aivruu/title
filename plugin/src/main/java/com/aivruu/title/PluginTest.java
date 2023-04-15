2266131fbf8734647626dd1d8108abf3f83bfb7c
InitSync <benjaalejandro206@gmail.com>
2023-04-15 18:50:59 -0400

    Use 'AdaptManager#of' method and give 'AdaptHandler' for v1_8_R3, and add more cases to the switch block

--- plugin/src/main/java/com/aivruu/title/PluginTest.java
+++ plugin/src/main/java/com/aivruu/title/PluginTest.java
@@ -1,18 +1,18 @@
 package com.aivruu.title;
 
+import com.aivruu.title.adapt.v1_8_R3.AdaptHandler;
 import com.aivruu.title.model.AdaptManager;
 import com.aivruu.title.model.TitleManager;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PluginTest extends JavaPlugin {
-	private AdaptManager adaptManager;
 	private TitleManager titleManager;
 	
 	@Override
 	public void onLoad() {
-		adaptManager = Title.newAdaptManager();
-		if (!adaptManager.find()) return;
+		final AdaptManager adaptManager = Title.newAdaptManager();
+		if (!adaptManager.of(new AdaptHandler())) return;
 		
 		titleManager = Title.newTitleManager(adaptManager.get());
 	}
@@ -36,6 +36,12 @@ public final class PluginTest extends JavaPlugin {
 					break;
 				case "tab":
 					titleManager.setHeaderAndFooter(player, "Header Example", "Footer Example");
+					break;
+				case "header":
+					titleManager.setHeader(player, "Header Test Example");
+					break;
+				case "footer":
+					titleManager.setFooter(player, "Footer Test Example");
 			}
 			
 			return false;
