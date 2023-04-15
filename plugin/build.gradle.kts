90a548b4780c97c68049a3b37dd251161b03ca41
InitSync <benjaalejandro206@gmail.com>
2023-04-14 21:28:23 -0400

    Add plugin-yml minecrell gradle plugin

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
 	id("java")
+    id("net.minecrell.plugin-yml.bukkit") version("0.5.3")
 }
 
 repositories {
@@ -12,4 +13,21 @@ dependencies {
 	compileOnly("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT")
 	
 	implementation(project(":base"))
+}
+
+bukkit {
+    var directory = property("group") as String
+
+    name = "PluginTest"
+    main = "$directory.PluginTest"
+    authors = listOf("InitSync")
+
+    apiVersion = "1.13"
+    version = "0.0.1"
+
+    commands {
+        register("test") {
+            description = "Test command."
+        }
+    }
 }
