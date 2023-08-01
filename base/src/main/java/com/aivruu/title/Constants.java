0d9c1e2506ebb7a547518b2c52101d346b5b9bc5
InitSync <benjaalejandro206@gmail.com>
2023-08-01 10:35:01 -0400

    feat: add class to keep server-version on usage

---
+++ base/src/main/java/com/aivruu/title/Constants.java
@@ -0,0 +1,7 @@
+package com.aivruu.title;
+
+import org.bukkit.Bukkit;
+
+public class Constants {
+	public static final int SERVER_VERSION = Integer.parseInt(Bukkit.getBukkitVersion().split("-")[0].split("\\.")[1]);
+}
